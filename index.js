6436a0942890b3b1606bb97d3cae42064c61d4c9
Huy Nguyễn <huyndx@outlook.com>
2020-11-19 22:27:52 +0700

    Update

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const minify = require('html-minifier').minify;
 const ejs = require('ejs');
 module.exports =  (viewfile, option, callback) => {
-    ejs.renderFile(viewfile, option, {}, (e, o) => {
-        callback(minify(o, {
-            removeTagWhitespace: true,
-            removeComments: true,
-            collapseInlineTagWhitespace: true,
-            removeOptionalTags: true,
-            useShortDoctype: true,
-            removeEmptyAttributes: true,
-            collapseWhitespace: true,
-            minifyCSS:true,
-            minifyJS:true
-        }));
-    });
+    if(typeof viewfile === "object"){
+        if(typeof callback === "undefined"){callback={};}
+        ejs.renderFile("views/" + option + ".ejs", callback, {}, (e, o) => {
+            viewfile.send(minify(o, {
+                removeTagWhitespace: true,
+                removeComments: true,
+                collapseInlineTagWhitespace: true,
+                removeOptionalTags: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                collapseWhitespace: true,
+                minifyCSS: true,
+                minifyJS: true
+            }));
+        });
+    }else {
+        ejs.renderFile(viewfile, option, {}, (e, o) => {
+            callback(minify(o, {
+                removeTagWhitespace: true,
+                removeComments: true,
+                collapseInlineTagWhitespace: true,
+                removeOptionalTags: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                collapseWhitespace: true,
+                minifyCSS: true,
+                minifyJS: true
+            }));
+        });
+    }
 };
